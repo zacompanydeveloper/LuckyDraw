19a858f05c82fc99f35de65c6358300bd1616302
YTR <ytr@za.com.mm>
2025-11-08 13:40:48 +0630

    prize winner png export

--- src/pages/TicketWinner.vue
+++ src/pages/TicketWinner.vue
@@ -4,9 +4,8 @@
 
             <div class="flex justify-between items-center">
                 <h2 class="text-2xl font-bold text-[#2E3192]">{{ $t('ticket_winner') }}</h2>
-                <Button type="button" v-tooltip.top="$t('download_winners_list')"
-                    iconPos="right" icon="pi pi-download" @click="exportData" raised
-                    style="background-color: #2E3192;" />
+                <Button type="button" v-tooltip.top="$t('download_winners_list')" iconPos="right" icon="pi pi-download"
+                    @click="exportData" raised style="background-color: #2E3192;" />
             </div>
             <!-- Table -->
             <div class="card mt-2">
@@ -55,6 +54,16 @@
                     <Column headerStyle="background-color: #2E3192; color: white;" class="table-header"
                         field="serial_code" :header="$t('serial_code')" />
 
+                    <Column headerStyle="background-color: #2E3192; color: white;" :header="$t('action')"
+                        class="w-24 table-header" :headerStyle="{ textAlign: 'right' }">
+                        <template #body="{ data }">
+                            <div class="flex justify-center items-center">
+                                <Button v-tooltip.top="$t('export_png')" icon="pi pi-download" @click="exportPNG(data)"
+                                    severity="success" outlined rounded />
+                            </div>
+                        </template>
+                    </Column>
+
                     <template #empty>
                         <div class="p-4 text-center text-gray-500">
                             {{ $t('no_records_found') }}
@@ -70,12 +79,55 @@
         <div v-else>
             <NotFound />
         </div>
+
+        <!-- Winner Dialog -->
+         <div class="export-container-wrapper absolute top-[-10000px] left-[-10000px]">
+        <div ref="imageContainer"
+            :style="{ backgroundImage: `url(${bgImage})` }"
+            class="winner-dialog flex items-center justify-center audiowide-regular w-full max-w-[1000px]  aspect-square py-6">
+            <div
+                class="flex flex-col justify-between items-center gap-4 w-full h-full text-center text-[#080D88] relative">
+                <div class="w-full flex justify-between">
+                    <img src="@/assets/images/sweetyhome_logo.png" alt="" srcset="">
+                    <img src="@/assets/images/30_years_anni.png" alt="" srcset="">
+                </div>
+                <div class="w-full max-w-[820px]">
+                    <h1 class="text-[#080D88] text-[75.85px] uppercase mb-4">Congratulation</h1>
+                    <h3 class="text-[55.35px] uppercase">Winner is</h3>
+                </div>
+                <div class="flex flex-col items-center gap-4 w-full max-w-[820px]">
+                    <p class="text-[49.91px] flex items-center font-semibold uppercase py-4 px-3 inter-custom text-white bg-[#2E3192]">
+                        {{ selectedCustomer?.shop_name }} | {{ selectedCustomer?.shop_township }}
+                    </p>
+                    <h1 class="text-[65.56px] font-normal">{{ selectedCustomer?.name }}</h1>
+                    <p class="text-[36px] text-[#2E3192] inter-custom">
+                        {{ selectedCustomer?.township }}
+                    </p>
+                    <p class="text-[36px] font-light text-[#2E3192] inter-custom">
+                        {{ selectedCustomer?.nrc }}
+                    </p>
+                    <div class="relative w-full min-w-[610px] mt-6">
+                        <img :src="selectedPrize?.image" alt="prize"
+                            class="w-50 object-contain absolute bottom-2 left-2" />
+                        <h1
+                            class="text-[61.74px] inter-custom font-semibold text-white text-shadow-lg ps-50 rounded-lg">
+                            {{ selectedPrize?.name }} {{ selectedPrize?.color }}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+
     </div>
 </template>
 
 <script setup>
 import { onMounted, reactive, ref } from "vue";
 import { useToast } from "primevue/usetoast";
+import bgImage from "@/assets/images/winner_export_bg.png";
+import examplePrize from "@/assets/images/prize.png";
+import html2canvas from "html2canvas";
 import { useI18n } from "vue-i18n";
 import helper from "@/helper";
 import backend from "@/api/backend";
@@ -83,6 +135,20 @@ import NotFound from "@/views/NotFound.vue";
 
 const toast = useToast();
 const { t } = useI18n();
+
+const imageContainer = ref(null);
+const selectedCustomer = ref({
+    name: 'Ye Thura',
+    shop_name: 'Pro(1)',
+    shop_township: 'Botathaung',
+    township: 'Sanchaung',
+    nrc: '12/KaLaNa(N)123456',
+});
+const selectedPrize = ref({
+    name: 'Iphone 16 Pro',
+    color: 'White Titanium',
+    image: examplePrize,
+});
 
 const isMobile = helper.isMobile();
 
@@ -156,6 +222,58 @@ const exportData = async () => {
         });
     } finally {
         loading.table = false;
+    }
+};
+
+// png export for winner info
+const exportPNG = async (data) => {
+    selectedCustomer.value = {
+        name: data.customer_name,
+        shop_name: data.shop_name,
+        shop_township: data.shop_township,
+        township: data.township,
+        nrc: data.nrc,
+    };
+
+    selectedPrize.value = {
+        name: data.prize.name,
+        color: data.prize.color || '',
+        image: data.prize.image.url,
+    };
+    loading.table = true;
+    console.log('Selected Customer:', selectedCustomer.value);
+    console.log('Selected Prize:', selectedPrize.value);
+    try {
+        // need to settime here to wait for DOM update
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        const captureElement = imageContainer.value;
+
+        // do i need to wait here ?
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+
+        console.log('logging the capture element:', captureElement);
+        
+        html2canvas(captureElement, {
+            useCORS: true,
+            allowTaint: true,
+        }).then((canvas) => {
+            const image = canvas.toDataURL("image/png");
+
+            const link = document.createElement("a");
+            link.href = image;
+            link.download = "winner.png";
+            link.click();
+        });
+        loading.table = false;
+    } catch (error) {
+        loading.table = false;
+        console.error("Error exporting PNG:", error);
+        toast.add({
+            severity: "error",
+            summary: t('error'),
+            detail: error.response?.data?.message || t('error_occurred'),
+            life: 5000
+        });
     }
 };
 
