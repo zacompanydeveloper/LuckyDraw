bf4ed5e166674b4853ba678fe7cbe9c57ecec977
Nanda Aung <na@za.com.mm>
2025-11-08 10:17:51 +0630

    upt winner.vue

--- src/components/Winners.vue
+++ src/components/Winners.vue
@@ -9,70 +9,38 @@
 
     <!-- Winners List -->
     <div ref="scrollContainer" class="mt-2 flex-1 overflow-hidden px-6 relative text-3xl font-bold">
-      <div
-        class="absolute top-0 left-0 right-0 transition-transform duration-500 ease-linear"
-        :style="{ transform: `translateY(-${scrollOffset}px)` }"
-      >
+      <div class="absolute top-0 left-0 right-0 transition-transform duration-500 ease-linear"
+        :style="{ transform: `translateY(-${scrollOffset}px)` }">
         <!-- Original Winners -->
-        <div
-          v-for="(winner, index) in winners"
-          :key="winner.id || winner.nrc"
-          class="grid grid-cols-4 gap-1 mt-1 text-center items-stretch text-[#2E3192]"
-        >
-          <p
-            class="p-6 flex items-center justify-center"
-            :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'"
-          >
+        <div v-for="(winner, index) in winners" :key="winner.id || winner.nrc"
+          class="grid grid-cols-4 gap-1 mt-1 text-center items-stretch text-[#2E3192]">
+          <p class="p-6 flex items-center justify-center" :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'">
             {{ winner.customer_name }}
           </p>
-          <p
-            class="p-6 flex items-center justify-center"
-            :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'"
-          >
-            {{ winner.township }}
+          <p class="p-6 flex items-center justify-center" :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'">
+            {{ winner.business_type == 'business' ? winner.township : winner.shop_name }}
           </p>
-          <p
-            class="p-6 flex items-center justify-center"
-            :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'"
-          >
+          <p class="p-6 flex items-center justify-center" :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'">
             {{ winner.nrc }}
           </p>
-          <p
-            class="p-6 flex items-center justify-center"
-            :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'"
-          >
+          <p class="p-6 flex items-center justify-center" :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'">
             {{ winner.prize?.name || '-' }} <br> {{ winner.prize?.color || '' }}
           </p>
         </div>
 
         <!-- Duplicate Winners for seamless scroll -->
-        <div
-          v-for="(winner, index) in winners"
-          :key="`duplicate-${winner.id || winner.nrc}`"
-          class="grid grid-cols-4 gap-1 mt-3 text-center items-stretch text-[#2E3192]"
-        >
-          <p
-            class="p-6 flex items-center justify-center"
-            :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'"
-          >
+        <div v-for="(winner, index) in winners" :key="`duplicate-${winner.id || winner.nrc}`"
+          class="grid grid-cols-4 gap-1 mt-3 text-center items-stretch text-[#2E3192]">
+          <p class="p-6 flex items-center justify-center" :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'">
             {{ winner.customer_name }}
           </p>
-          <p
-            class="p-6 flex items-center justify-center"
-            :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'"
-          >
-            {{ winner.township }}
+          <p class="p-6 flex items-center justify-center" :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'">
+            {{ winner.business_type == 'business' ? winner.township : winner.shop_name }}
           </p>
-          <p
-            class="p-6 flex items-center justify-center"
-            :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'"
-          >
+          <p class="p-6 flex items-center justify-center" :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'">
             {{ winner.nrc }}
           </p>
-          <p
-            class="p-6 flex items-center justify-center"
-            :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'"
-          >
+          <p class="p-6 flex items-center justify-center" :class="index % 2 === 0 ? 'bg-[#D8EBFF]' : 'bg-[#C8E0FF]'">
             {{ winner.prize?.name || '-' }} <br> {{ winner.prize?.color || '' }}
           </p>
         </div>
@@ -111,7 +79,7 @@ onMounted(() => {
     if (scrollOffset.value >= contentHeight) {
       scrollOffset.value = 0; // seamless reset
     }
-  }, 100); // adjust speed here
+  }, 30); // adjust speed here
 });
 
 onUnmounted(() => {
